12370dc471febd2361c8691feed7ed61c2cfac67
gm4vin2tech <gm4vin2tech@gmail.com>
2022-01-21 14:33:54 +0530

    added static files

---
+++ travelsite/accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views  #importing views.py file
+
+
+urlpatterns = [
+    # path("register", views.register, name="register"), #register
+    # path("login", views.login, name="login"), #login
+    # path("logout", views.logout, name="logout"), #logout
+]
